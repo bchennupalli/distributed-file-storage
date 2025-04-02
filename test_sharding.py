e79e16aef5b02f993bc8587bfc7c61fd2375eca8
lbugata <bchennupalli@binghamton.edu>
2025-04-01 23:39:40 -0400

    Initial commit: Setup basic project structure in distributed-file-storage

--- test_sharding.py
+++
@@ -1,6 +0,0 @@
-from utils.file_sharding import shard_file
-
-# Split the file into 10-byte chunks (for testing)
-shard_ids = shard_file("large_file.txt", chunk_size=10)
-
-print("Shard IDs:", shard_ids)
